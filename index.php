6c6da0dbc8ffca6f521e88361144ae200d0a9579
xuyuanfang <xuyuanfang@vip.qq.com>
2016-10-29 06:24:47 +0800

    update

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <title>WHU搜索导航——只为更简洁</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0" /> 
-<link href="style.css" rel="stylesheet" type="text/css" />
-<script class="cssdeck" src="jquery.min.js"></script>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<script class="cssdeck" src="js/jquery.min.js"></script>
 </head>
 
 <body>
